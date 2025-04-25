2672cfb3bffa827649a51fac15e6328007c22519
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-04-25 13:00:05 +0300

    Update api.js with the render backend URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://task-manager-server-eojg.onrender.com";
 
 const handleApiError = (error) => {
   if (error.response) {
